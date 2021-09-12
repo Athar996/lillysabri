974c6ac6f457c4ff6405fed684181702086582f5
Athar Mohammad <82324903+Athar996@users.noreply.github.com>
2021-09-12 23:07:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function signup()
+{
+    window.open("https://mailchi.mp/leanwithlilly/freeworkouttimetable","_blank");
+}
+
+function subsbtn()
+{
+    window.open("https://www.youtube.com/channel/UCTsM1dSAiXqiV5oZjuNw_Bg","_blank");
+}
+
+function buynow()
+{
+    window.open("https://www.leanwithlilly.com/products/the-anti-inflammatory-cook-book/","_blank");
+}
+function gym()
+{   
+    window.open("https://uk.gymshark.com/collections/new-releases/womens?utm_source=ACA&utm_medium=3728830_Lilly+Sabri&utm_campaign=instagram&ranMID=45837&ranEAID=8hecCNwSw7c&ranSiteID=8hecCNwSw7c-CeQuSV5aj9SpJAbrQJ8IMA","_blank");
+}
+function buynow2()
+{
+    window.open("https://www.leanwithlilly.com/products/video-8-week-lean-transformation-guide","_blank");
+}
+
+function shopnow()
+{
+    window.open("https://www.leanwithlilly.com/collections/resistance-bands/","_blank")
+}
